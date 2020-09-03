71f71edf422abcb1acea27652d35271d09cbe8a0
Tracy Vy <tracyvy88@gmail.com>
2020-09-03 11:50:48 -0700

    Updates

---
+++ precache-manifest.6acbd3f90e42bf564c20c3ee10e042a1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "026caf5101e937f5bd5f3fad57e1fd53",
+    "url": "/react_youtube_app/index.html"
+  },
+  {
+    "revision": "df08a1c87740a44f0f87",
+    "url": "/react_youtube_app/static/css/main.ce4aab6c.chunk.css"
+  },
+  {
+    "revision": "9bca1de5566090104b72",
+    "url": "/react_youtube_app/static/js/2.dbfe0e3b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_youtube_app/static/js/2.dbfe0e3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df08a1c87740a44f0f87",
+    "url": "/react_youtube_app/static/js/main.59ab2183.chunk.js"
+  },
+  {
+    "revision": "98ac486ccfe1e6f9975a",
+    "url": "/react_youtube_app/static/js/runtime-main.e2ca3d55.js"
+  }
+]);
